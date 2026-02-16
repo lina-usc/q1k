1a8cdc811b364dac8df9c0bdff5d7541b2c94723
Christian O'Reilly <oreillyc@narval2.narval.calcul.quebec>
2026-02-16 12:56:42 -0500

    Update epoch fetching.

--- q1k/io.py
+++ q1k/io.py
@@ -1,20 +1,25 @@
 from pathlib import Path
 
 
-def get_project_site_path(site, root=None):
+def get_project_site_path(root=None):
     if root is None:
         root = Path.home() / "projects" / "def-emayada"
-    return root / "q1k" / "experimental" / site
-    
+    else:
+        root = Path(root)
+    return root / "q1k" / "experimental" 
 
-def get_epoch_path(task, site, root=None):
-    project_site_path = get_project_site_path(site, root)
-    pylossless_path = project_site_path / "derivatives" / "pylossless"
-    postproc_path = pylossless_path / "derivatives" / "postproc"
+
+def get_preproc_path(root=None):
+    project_site_path = get_project_site_path(root)
+    return  project_site_path / "derivatives" / "pylossless"
+
+
+def get_epoch_path(task, root=None):
+    pylossless_path = get_preproc_path(root)
+    postproc_path = pylossless_path / "derivatives" / "sync_loss" / "derivatives" / "segment"
     return postproc_path / 'epoch_fif_files' / task
 
-    
-def get_epoch_files(*args, **kwargs):
-    epoch_path = get_epoch_path(*args, **kwargs)
-    return list(epoch_path.glob('*epochs.fif'))
 
+def get_epoch_files(*args, file_pattern="*eeg_epo.fif", **kwargs):
+    epoch_path = get_epoch_path(*args, **kwargs)
+    return list(epoch_path.glob(file_pattern))
